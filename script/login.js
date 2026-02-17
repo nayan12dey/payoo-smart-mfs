f5fbaa625f04ba032a6dd1f9ae6ab7a669c5e413
Nayan Dey <ndeynayan123@gmail.com>
2026-02-18 03:13:39 +0530

    Add input IDs and implement login functionality in JavaScript

--- script/login.js
+++ script/login.js
@@ -1,1 +1,26 @@
-console.log("Login functionality comming")
+// console.log("Login functionality comming")
+
+document.getElementById("login-btn").addEventListener("click", function(){
+    // 1. get the mobile number
+    const numberInput = document.getElementById("input-number")
+    const contactNumber = numberInput.value;
+    console.log(contactNumber)
+
+    // 2. get the pin
+    const inputPin = document.getElementById("input-pin");
+    const pin = inputPin.value;
+    console.log(pin);
+
+    // 3. match pin & mobile number
+    if(contactNumber == "8014565780" && pin == "1234"){
+
+        // 3-1 true:::>> alert> homepage
+        alert("login sucessfully")
+    }
+    else{
+
+        // 3-2 false:::>> alert > return
+        alert("login failed");
+        return;
+    }
+})
